eb08eee986796c683ffa53391602a35a35c00713
josh legard <java@unicauca.edu.co>
2021-09-12 03:37:20 -0500

    NPM install

--- Seccion 4/app.js
+++ Seccion 4/app.js
@@ -1,13 +1,16 @@
 
 const {crearFile} = require('./helpers/multiplicar');
 console.clear();
+console.log(process.argv);
+const [ , , arg3 = 'number=5'] = process.argv;
+const [ , number] = arg3.split('=');
+console.log( number );
 
-const number = 9;
 crearFile ( number )
     .then(crearFile => console.log(crearFile,'Promise response: Created'))
     .catch(err => console.log(err, 'Failed to create'));
 
-//fs.writeFile( `tabla-${number}.txt`, salida, (err) => {
+    //fs.writeFile( `tabla-${number}.txt`, salida, (err) => {
 //if (err) throw err;
 //    console.log(`archivo tabla-${number}.txt creado`);
 //})
