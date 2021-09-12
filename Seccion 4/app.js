db088f32e87157aa89caf7cae3a5acc6a7a9ce5c
josh legard <java@unicauca.edu.co>
2021-09-12 15:32:46 -0500

    reinstalled npm

--- Seccion 4/app.js
+++ Seccion 4/app.js
@@ -1,12 +1,34 @@
 
+//const { argv } = require('yargs');
 const {crearFile} = require('./helpers/multiplicar');
+const argv = require('yargs')
+            .option('n',{
+                alias: 'numero', 
+                type: 'number', 
+                demandOption: true 
+            })
+            .check((argv, options) => {
+                if ( isNaN(argv.n)){
+                    throw 'Error: Solo se pueden ingresar números'
+                }
+                return true;
+            })
+            .option('l', {
+                alias: 'listar', 
+                type: 'boolean',
+                default: false  
+            })
+            .argv;
 console.clear();
-console.log(process.argv);
-const [ , , arg3 = 'number=5'] = process.argv;
-const [ , number] = arg3.split('=');
-console.log( number );
+//console.log(process.argv);
+console.log(argv);
+//console.log('number: yargs', argv.n);
+//console.log('number: yargs', argv.number);
+//const [ , , arg3 = 'number=5'] = process.argv;
+//const [ , number] = arg3.split('=');
+//console.log( number );
 
-crearFile ( number )
+crearFile ( argv.n, argv.l )
     .then(crearFile => console.log(crearFile,'Promise response: Created'))
     .catch(err => console.log(err, 'Failed to create'));
 
--- Seccion 4/helpers/multiplicar.js
+++ Seccion 4/helpers/multiplicar.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
-const crearFile = ( number ) => {
+
+const crearFile = ( number, listar =    false ) => {
     return new Promise((resolve, reject) => {
-        console.log('***********************');
-        console.log('Tabla del: ', number);
-        console.log('***********************');
+       
         
         let salida = '';
         for (let i = 1; i <11; i++) {
             salida += `${number} x ${i} = ${ number * i}\n`
             //console.log(`${number} x ${i}=`,number*i)    
         }
-        (salida)
-        ?resolve(salida)
-        :reject(`No ha sido posible para el número dado`);
-        
+
+        if (listar) {
+            console.log('***********************');
+            console.log('Tabla del: ', number);
+            console.log('***********************');
+            (salida)
+            ?resolve(salida)
+            :reject(`No ha sido posible para el número dado`);
+        }
         fs.writeFileSync( `./resultados/tabla-${number}.txt`, salida);
         console.log(`El archivo tabla-${number}.txt ha sido creado. Ver carpeta de resultados.`);
-
     });
 }
 
